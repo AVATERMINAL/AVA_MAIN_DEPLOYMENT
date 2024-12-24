864f9dca5778ae1f3128fbb799534dbf60179fb5
AVA Terminal <swedishnord123@googlemail.com>
2024-12-23 20:06:58 -0600

    AVA_AGENT

---
+++ cava/nightwatch/generator/c/instrumentation.py
@@ -0,0 +1,70 @@
+from nightwatch.model import Type, Argument, Function
+
+
+# TODO: Split this function into it's three conceptual elements.
+def timing_code_guest(selector: str, name: str, enabled: bool) -> str:
+    if not enabled:
+        return ""
+    return f"""#ifdef AVA_PRINT_TIMESTAMP
+        {{
+            struct timeval ts_{name}_timing;
+            gettimeofday(&ts_{name}_timing, NULL);
+            printf("Guestlib: {name} {selector} at : %ld s, %ld us\\n", ts_{name}_timing.tv_sec, ts_{name}_timing.tv_usec);
+        }}
+    #endif\n"""
+
+
+def time_stamp_begin(name: str, enabled: bool) -> str:
+    if not enabled:
+        return ""
+    return f"""#ifdef __AVA_ENABLE_STAT
+               auto {name}_begin_ts = ava::GetMonotonicNanoTimestamp();
+    #endif\n"""
+
+
+def time_stamp_end(name: str, enabled: bool) -> str:
+    if not enabled:
+        return ""
+    return f"""#ifdef __AVA_ENABLE_STAT
+    auto {name}_end_ts = ava::GetMonotonicNanoTimestamp();
+    #endif\n"""
+
+
+def report_type_alloc_resources(ty: Type) -> str:
+    ret = ""
+    for resource, amount in ty.allocates_resources.items():
+        ret += f"""nw_report_storage_resource_allocation("{resource}", {amount});\n"""
+    for resource, amount in ty.deallocates_resources.items():
+        ret += f"""nw_report_storage_resource_allocation("{resource}", -{amount});\n"""
+    return ret
+
+
+def report_alloc_resources(arg: Argument) -> str:
+    ret = ""
+    for ty in arg.contained_types:
+        ret += report_type_alloc_resources(ty)
+    return ret
+
+
+def report_consume_resources(f: Function) -> str:
+    ret = ""
+    for resource, amount in f.consumes_resources.items():
+        ret += f"""nw_report_throughput_resource_consumption("{resource}", {amount});\n"""
+    ret += """
+    #ifdef AVA_API_FUNCTION_CALL_RESOURCE
+        nw_report_throughput_resource_consumption("ava_api_function_call", 1);
+    #endif
+    """
+    return ret
+
+
+def timing_code_worker(location: str, name: str, enabled: bool) -> str:
+    if not enabled:
+        return "".strip()
+    return f"""#ifdef AVA_PRINT_TIMESTAMP
+        {{
+            struct timeval ts_{name}_timing;
+            gettimeofday(&ts_{name}_timing, NULL);
+            printf("Worker: {name} {location} at : %ld s, %ld us\\n",ts_{name}_timing.tv_sec, ts_{name}_timing.tv_usec);
+        }}
+        #endif\n"""
---
+++ cava/nightwatch/generator/c/worker.py
@@ -0,0 +1,39 @@
+from typing import Tuple
+
+from nightwatch.generator.c.stubs import function_implementation
+from nightwatch.generator.c.command_handler import handle_command_function, handle_command_header
+from nightwatch.model import API, lines
+
+
+def handle_call(api: API) -> str:
+    return handle_command_function(api, list(api.real_functions) + list(api.callback_functions), api.callback_functions)
+
+
+def source(api: API) -> Tuple[str, str]:
+    prelude = f"""
+#define __AVA__ 1
+#define ava_is_worker 1
+#define ava_is_guest 0
+
+#include "worker.h"
+
+#undef AVA_BENCHMARKING_MIGRATE
+
+{handle_command_header(api)}
+
+void init_worker(void) {{
+    __handle_command_{api.identifier.lower()}_init();
+    {api.worker_init_epilogue};
+}}
+"""
+    stubs = f"""
+////// API function stub implementations
+
+#define __chan nw_global_command_channel
+
+{lines(function_implementation(f) for f in api.callback_functions)}
+"""
+
+    function = handle_call(api)
+
+    return api.c_worker_spelling, prelude + stubs + function
